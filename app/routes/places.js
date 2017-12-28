2170fa3e54a0be47e646cc0c87f2759c79baba3b
catasaw <catalina89@msn.com>
2017-12-28 17:40:35 +0100

    Add place listing component with map

--- app/routes/places.js
+++ app/routes/places.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+  return [{
+    id: '1',
+    title: 'Abandoned populated place',
+    city: 'Pröbsten'
+  }, {
+    id: '2',
+    title: 'Abandoned populated place',
+    city: 'Boestlingen'
+  }, {
+    id: '3',
+    title: 'Abandoned populated place',
+    city: 'Benhorn'
+  }];
+}
 });
